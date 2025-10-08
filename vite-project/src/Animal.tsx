5e5e9ba181cc506ea507210b9a17de3031a11ade
Douglas Mariano <douglas.mariano@estudante.ufjf.br>
2025-10-07 22:03:43 -0300

    adciona animal props

---
+++ vite-project/src/Animal.tsx
@@ -0,0 +1,19 @@
+import "./Animal.css"
+
+type AnimalProps = {
+    icone?: string;
+    nome?: string;
+    peso?: number;
+    emExtincao?: boolean;
+}
+
+export default function Animal({icone="?", nome="Desconhecido", peso=0.1, emExtincao=false}:AnimalProps){
+    return (
+        <div className="animal"> 
+            <div>{icone}</div>
+            <div>{nome}</div>
+            <div>{peso.toLocaleString()} Kg</div>
+            <div>{emExtincao}</div>
+        </div>
+    );    
+}
---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import Animal from './Animal'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <Animal/>
+    </div>
+  )
+}
+export default App
