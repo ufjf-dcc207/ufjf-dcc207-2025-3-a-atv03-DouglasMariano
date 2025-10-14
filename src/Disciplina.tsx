8f565dd2160dfac8b2274d6eae68f819fa5fa1e7
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2025-10-14 20:18:58 -0300

    criado o hello world

---
+++ src/Disciplina.tsx
@@ -0,0 +1,10 @@
+function Disciplina(){
+    return (
+        <div className="disciplina">
+            <div>DCC207</div>
+            <div>Dsenvolvimento Web Front-End</div>
+        </div>
+    )
+}
+
+export default Disciplina;
