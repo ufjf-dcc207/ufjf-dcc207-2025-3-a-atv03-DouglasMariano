cd8059ba7b8fcc7b21b40202a61377cb0602f0bf
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2025-10-14 19:53:43 -0300

    criado o gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/ufjf-dcc207-2025-3-a-atv03-DouglasMariano",
   plugins: [react()],
 })
